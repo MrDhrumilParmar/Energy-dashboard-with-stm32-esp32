6b7da8d57a3eb518cb1f17a8131d2e3ea9eb2bae
MrDhrumil <dhrumilparmar3478@gmail.com>
2025-12-15 03:27:48 +0530

    Stm V2 successfully updated
    new type of uart string will be transmitted from USART1

---
+++ STM32_RTOS/015Task2_ADC_USART/Debug/Common/Third_Party/FreeRTOS/portable/MemMang/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/ST/STM32CubeIDE_1.19.0/STM32CubeIDE/file/D/RTOS%20STM%20Project/RTOS\ Workspace/Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.c 
+
+OBJS += \
+./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.o 
+
+C_DEPS += \
+./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.o: C:/ST/STM32CubeIDE_1.19.0/STM32CubeIDE/file/D/RTOS%20STM%20Project/RTOS\ Workspace/Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.c Common/Third_Party/FreeRTOS/portable/MemMang/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"D:/RTOS STM Project/RTOS Workspace/Common/Third_Party/FreeRTOS/include" -I"D:/RTOS STM Project/RTOS Workspace/Common/Third_Party/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/RTOS STM Project/RTOS Workspace/Common/Third_Party/SEGGER/Config" -I"D:/RTOS STM Project/RTOS Workspace/Common/Third_Party/SEGGER/OS" -I"D:/RTOS STM Project/RTOS Workspace/Common/Third_Party/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Common-2f-Third_Party-2f-FreeRTOS-2f-portable-2f-MemMang
+
+clean-Common-2f-Third_Party-2f-FreeRTOS-2f-portable-2f-MemMang:
+	-$(RM) ./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.cyclo ./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.d ./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.o ./Common/Third_Party/FreeRTOS/portable/MemMang/heap_4.su
+
+.PHONY: clean-Common-2f-Third_Party-2f-FreeRTOS-2f-portable-2f-MemMang
+
